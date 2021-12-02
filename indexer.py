63a90658e29b91f5e4252d919c709a55313749ae
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-12-01 19:18:21 -0500

    Added indexer ability to parse dataset into dict

--- indexer.py
+++ indexer.py
@@ -27,8 +27,29 @@ import csv
 def parse_ratings() -> dict:
     csvfile = open('../ml-latest-small/ratings.csv', newline='')
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    database = {}
     for row in reader:
-        print(row[0].split(","))
+        # line = [userId,movieId,rating]
+        line = row[0].split(",")[:-1]
+
+        # Skip the first line of the CSV
+        if not line[0].isnumeric():
+            continue
+
+        userId = int(line[0])
+        movieId = int(line[1])
+        rating = float(line[2])
+
+        # Create new user in database with its value as the movie/rating
+        if userId not in database.keys():
+            database[userId] = { movieId : rating }
+
+        # Update the userId with the related movie and rating
+        else:
+            database[userId][movieId] = rating
+
+    return database
 
 parse_ratings()
 
