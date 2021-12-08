cd3f41ec9da7e7fa6f0898573623008255ef3876
Tommy Le <tomtom@Tommys-MacBook-Pro.local>
2021-12-07 19:12:43 -0500

    skipped csv header and closed parsing function

--- server/indexer.py
+++ server/indexer.py
@@ -27,18 +27,17 @@ DATASTRUCTURE:
     }
 """
 
-def parse_ratings(csv_name : str):
+
+def parse_ratings(csv_name: str):
     csvfile = open(csv_name, newline='')
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    
+
     database = {}
+
+    next(reader, None)  # Skips header row
     for row in reader:
         # line = [userId,movieId,rating]
         line = row[0].split(",")[:-1]
-
-        # Skip the first line of the CSV
-        if not line[0].isnumeric():
-            continue
 
         userId = int(line[0])
         movieId = int(line[1])
@@ -46,22 +45,25 @@ def parse_ratings(csv_name : str):
 
         # Create new user in database with its value as the movie/rating
         if userId not in database.keys():
-            database[userId] = { movieId : rating }
+            database[userId] = {movieId: rating}
 
         # Update the userId with the related movie and rating
         else:
             database[userId][movieId] = rating
 
+    csvfile.close()
     return database
 
-def intersect(x : dict, y : dict) -> list:
+
+def intersect(x: dict, y: dict) -> list:
     """
     Returns the intersection between two user movie recommendations have at least
     one common movie rated.
     """
     return list(set(x.keys()) & set(y.keys()))
 
-def union(x : list, y : dict) -> list:
+
+def union(x: list, y: dict) -> list:
     """
     Returns the union between two user movie recommendations
     x: List of similar movies
@@ -72,23 +74,27 @@ def union(x : list, y : dict) -> list:
 # user_input : {m1 : 1, m2: 2, m3: 3}
 # This function will iterate through database and find user matches
 # Returns a dict of similar users: {u1: sim_value, u2: sim_value}
-def find_sim(user_input : dict, database : dict) -> dict:
+
+
+def find_sim(user_input: dict, database: dict) -> dict:
     similar_users = {}
     for user in database.keys():
         check_intersect = intersect(user_input, database[user])
         if check_intersect != []:
-            sim = calc_sim(user_input, database[user],check_intersect)
+            sim = calc_sim(user_input, database[user], check_intersect)
             if sim > 0:
                 similar_users[user] = sim
 
     return similar_users
-    
+
 # x and y have form: {m1 : 1, m2: 2, m3: 3}
-def calc_sim(user_in : dict, b : dict, intersect : list) -> float:
+
+
+def calc_sim(user_in: dict, b: dict, intersect: list) -> float:
     usr_avg = 0
     b_avg = 0
     n = len(intersect)
-    
+
     for sim_movie in intersect:
         usr_avg += user_in[sim_movie]
         b_avg += b[sim_movie]
@@ -96,11 +102,10 @@ def calc_sim(user_in : dict, b : dict, intersect : list) -> float:
     # calculated average
     usr_avg = usr_avg / n
     b_avg = b_avg / n
-    
 
     sim = 0
-    denominator1 = 0 # sum of user_in rating squared
-    denominator2 = 0 # sum of user b rating squared
+    denominator1 = 0  # sum of user_in rating squared
+    denominator2 = 0  # sum of user b rating squared
     for sim_movie in intersect:
         sim += (user_in[sim_movie] - usr_avg)*(b[sim_movie] - b_avg)
         denominator1 += ((user_in[sim_movie] - usr_avg) ** 2)
@@ -113,16 +118,19 @@ def calc_sim(user_in : dict, b : dict, intersect : list) -> float:
 
 # Uses the similar_users dictionary to find all the movies
 # rated by any of the similar users
-def find_similar_movies(similar_users : dict, database : dict, user_in : dict) -> list:
+
+
+def find_similar_movies(similar_users: dict, database: dict, user_in: dict) -> list:
     sim_movies = []
     # build list of all movies that similar user have rated and input user has not
     for user in similar_users.keys():
         user_rated_movies = database[user]
         sim_movies = union(sim_movies, user_rated_movies)
-    
+
     return [x for x in sim_movies if x not in user_in.keys()]
 
-def compute_movie_recommendation(n_movies : list, similar_users : dict, database : dict):
+
+def compute_movie_recommendation(n_movies: list, similar_users: dict, database: dict):
     """
     Returns a dictionary of estimated movie rating for any movie that the main user has
     not rated yet, based on the set of similar user ratings for that specific movie.
@@ -141,14 +149,17 @@ def compute_movie_recommendation(n_movies : list, similar_users : dict, database
         rating = compute_aggregate(similar_users, database, movieId)
         # Remove zero rated movies
         if rating > 0:
-            movie_recommend[movieId] = rating # populate dict of recommended movies for user_in
-        
+            # populate dict of recommended movies for user_in
+            movie_recommend[movieId] = rating
+
     return get_top_recommended(movie_recommend, 20)
 
-def compute_aggregate(similar_users : dict, database : dict, movieId: int) -> float:
+
+def compute_aggregate(similar_users: dict, database: dict, movieId: int) -> float:
     # get list of userId's who are in similar_users and have rated movieId
-    valid_raters = [userId for userId in similar_users.keys() if movieId in database[userId].keys()]
-    
+    valid_raters = [userId for userId in similar_users.keys(
+    ) if movieId in database[userId].keys()]
+
     # Threshold filtering
     if len(valid_raters) < 2:
         return 0.0
@@ -165,23 +176,25 @@ def compute_aggregate(similar_users : dict, database : dict, movieId: int) -> fl
 
     return 0.0
 
-def get_top_recommended(recommendations : dict, n: int):
-    top_n = sorted(recommendations.items(), key=lambda item: item[1], reverse=True)[:n]
+
+def get_top_recommended(recommendations: dict, n: int):
+    top_n = sorted(recommendations.items(),
+                   key=lambda item: item[1], reverse=True)[:n]
     return dict(top_n)
+
 
 if __name__ == '__main__':
     database = parse_ratings('../data/ratings.csv')
-    user_in = {114709: 2, 4262: 4, 50872: 2, 6537: 5, 158872: 3, 6365: 4} # User's movie 
+    user_in = {114709: 2, 4262: 4, 50872: 2,
+               6537: 5, 158872: 3, 6365: 4}  # User's movie
     user_in = {1: 4, 2: 4, 3: 2, 4: 4, 5: 2, 6: 2}
     similar_users = find_sim(user_in, database)
     print(similar_users)
-    n_movies = find_similar_movies(similar_users,database,user_in)
-    recommendations = compute_movie_recommendation(n_movies, similar_users, database)
+    n_movies = find_similar_movies(similar_users, database, user_in)
+    recommendations = compute_movie_recommendation(
+        n_movies, similar_users, database)
     index = 1
     for movie in recommendations.keys():
-        print(f"Rank: {index}, Movie: {movie}, Rating: {recommendations[movie]}")
+        print(
+            f"Rank: {index}, Movie: {movie}, Rating: {recommendations[movie]}")
         index += 1
-    
-
-
-
